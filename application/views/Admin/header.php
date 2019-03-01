febc333df96237419fb4dd17c030473fb30ef262
Bhagyashree8 <bhagyashree@peaceinfotech.com>
2019-03-01 17:16:22 +0530

    ui develop branch pull

---
+++ application/views/Admin/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url();?>/assets/images/favicon.png">
+    <title>WEBUCATE</title>
+    <link href="<?php echo base_url();?>assets/vendors/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
+    <!-- This page CSS -->
+    <!-- chartist CSS -->
+    <link href="<?php echo base_url();?>assets/vendors/morrisjs/morris.css" rel="stylesheet">
+    <!--c3 CSS -->
+    <link href="<?php echo base_url();?>assets/vendors/c3-master/c3.min.css" rel="stylesheet">
+    
+    <!-- flot css -->
+    <link href="css/pages/float-chart.css" rel="stylesheet">
+     <link href="<?php echo base_url();?>/assets/vendors/summernote/dist/summernote.css" rel="stylesheet" />
+
+    <!--Toaster Popup message CSS -->
+    <link href="<?php echo base_url();?>assets/vendors/toast-master/css/jquery.toast.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="<?php echo base_url();?>main/css/master-stylesheet.css" rel="stylesheet">
+    <!-- You can change the theme colors from here -->
+    <link href="<?php echo base_url();?>main/css/colors/default.css" id="theme" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+
+<![endif]-->
+
+
+</head>
